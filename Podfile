50a57814d5b348132117f353245d81a2317db399
abdalrahman essam <abdalrahmanessam@abdalrahmansmbp>
2019-05-19 18:12:53 +0200

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'WAIS - Control Your Pharmacy' do
+ 
+  use_frameworks!
+
+pod'Firebase' 
+pod'Firebase/Core'
+pod'Firebase/Database'
+pod'Firebase/Auth'
+pod 'CountryPickerSwift'
+pod 'IBAnimatable'
+end
---
+++ build/Pods.build/Debug-iphoneos/Pods-Essam Atwah.build/DerivedSources/Pods_Essam_Atwah_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_Essam_AtwahVersionString[];
+ extern const double Pods_Essam_AtwahVersionNumber;
+
+ const unsigned char Pods_Essam_AtwahVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Essam_Atwah  PROJECT:Pods-1" "\n";
+ const double Pods_Essam_AtwahVersionNumber __attribute__ ((used)) = (double)1.;
